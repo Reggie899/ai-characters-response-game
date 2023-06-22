9a19128529dc79ddad8ea52e9d11941a7d4e0b5f
Reggie899 <regina.schmidt1212@gmail.com>
2023-06-22 22:35:56 +0200

    basic get request

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+//import loginRouter from './routes/login.js';
+//import emailview from './routes/emailview.js';
+//import { connectDB } from './helpers/dbConnect.js';
+//import mongoose from 'mongoose';
+
+const server = express();
+server.use(cors()); 
+server.use(express.json()); 
+
+//server.use('/', console.log("It's Britney Bitch"));
+server.get('/', (req, res) => res.send("It's Britney Bitch"))
+
+//server.use('/read', emailview);
+
+
+//connectDB(); 
+// mongoose.connection.on("open", () => {
+//         console.log("connected to db")
+// });
+// mongoose.connection.on("error", (error) => {
+// console.log("Connection to MongoDB has failed ", error.message);
+    
+//});
+
+const PORT = process.env.PORT || 5099;
+
+server.listen(PORT, () => console.log(`Server is listening to port ${PORT} and running`));
